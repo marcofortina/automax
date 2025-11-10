bbd8abc909b664c4f8acaba9a15d774956ca5e4c
Marco Fortina <marco_fortina@hotmail.it>
2025-11-10 13:10:24 +0100

    feat: implement PluginManager

--- src/automax/core/managers/__init__.py
+++ src/automax/core/managers/__init__.py
@@ -4,9 +4,11 @@ Manager subpackage for Automax.
 Includes:
 - ConfigManager
 - LoggerManager
+- PluginManager
 """
 
 from .config_manager import ConfigManager
 from .logger_manager import LoggerManager
+from .plugin_manager import PluginManager
 
-__all__ = ["ConfigManager", "LoggerManager"]
+__all__ = ["ConfigManager", "LoggerManager", "PluginManager"]
