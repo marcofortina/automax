a88ab318f6b394e4d7f37f5e29e9b53483acf480
Marco Fortina <marco_fortina@hotmail.it>
2025-11-10 13:07:30 +0100

    feat: implement LoggerManager

--- src/automax/core/managers/__init__.py
+++ src/automax/core/managers/__init__.py
@@ -3,8 +3,10 @@ Manager subpackage for Automax.
 
 Includes:
 - ConfigManager
+- LoggerManager
 """
 
 from .config_manager import ConfigManager
+from .logger_manager import LoggerManager
 
-__all__ = ["ConfigManager"]
+__all__ = ["ConfigManager", "LoggerManager"]
